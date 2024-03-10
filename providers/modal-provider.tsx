227fbd85be835e69658f1f9c53510712331a74fe
Christie Leung <christie@murphyshome.net>
2024-03-09 20:07:11 -0800

    Update modal-provider.tsx

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { GroupModal } from "@/components/modal/group-modal";
-import { RequestModal } from "@/components/modal/request-modal";
 import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
